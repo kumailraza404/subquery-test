5879c4681436446eea88328fa19826f4c3e4ca70
Khuzama Shahid <skhuzamashahid@gmail.com>
2021-12-25 15:41:32 +0500

    added mapping for westend

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -15,14 +15,13 @@ async function ensureAccounts(accountIds: string[]): Promise<void> {
 
 
 export async function handleTransfer(event: SubstrateEvent): Promise<void> {
-    const DOT_REDENOMINATION_BLOCK = 1248328
     const {
         event: {
             data: [from, to, amount],
         },
     } = event;
     const blockNo = event.block.block.header.number.toNumber();
-    const decimals = blockNo >= DOT_REDENOMINATION_BLOCK ?  BigInt("1" + "0".repeat(tokens.DOT.decimals.new)) :  BigInt("1" + "0".repeat(tokens.DOT.decimals.old))
+    const decimals = BigInt("1" + "0".repeat(tokens.WESTEND.decimals))
     const transformedAmount = (amount as Balance).toBigInt();
     const extrinsicHash = event.extrinsic.extrinsic.hash.toString();
     const timestamp = event.extrinsic.block.timestamp;
@@ -31,7 +30,7 @@ export async function handleTransfer(event: SubstrateEvent): Promise<void> {
 
     await ensureAccounts([from.toString(), to.toString()]);
 
-    transferInfo.token = tokens.DOT.name;
+    transferInfo.token = tokens.WESTEND.name;
     transferInfo.fromId = from.toString();
     transferInfo.toId = to.toString();
     transferInfo.timestamp = timestamp;
