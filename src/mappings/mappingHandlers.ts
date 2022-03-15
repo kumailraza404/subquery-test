772185419ad1cf5145d7cec160ffa9a09dc68cba
khuzama98 <skhuzamashahid@gmail.com>
2022-03-15 21:10:34 +0500

    feat: add fail tnxs

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,8 +1,12 @@
-import { SubstrateEvent } from "@subql/types";
+import { SubstrateEvent, SubstrateExtrinsic } from "@subql/types";
 import { Transfer } from "../types";
 import { Balance } from "@polkadot/types/interfaces";
 import { Account } from '../types/models/Account';
 import { tokens } from '../helpers/token'
+
+const { KARURA: {
+    name, decimals
+} } = tokens
 
 async function ensureAccounts(accountIds: string[]): Promise<void> {
     for (const accountId of accountIds) {
@@ -14,10 +18,54 @@ async function ensureAccounts(accountIds: string[]): Promise<void> {
 }
 
 
+function calculateFees(extrinsic: SubstrateExtrinsic): bigint {
+    let depositFees = BigInt(0);
+    let treasuryFees = BigInt(0);
+
+    const eventRecordWithdraw = extrinsic.events.find((event) => {
+        return event.event.method == "Withdraw" && event.event.section == "balances"
+    })
+
+    logger.info('records -->' + eventRecordWithdraw)
+    if (eventRecordWithdraw) {
+        const {
+            event: {
+                data: [accountid, fee]
+            }
+        } = eventRecordWithdraw
+
+        const extrinsicSigner = extrinsic.extrinsic.signer.toString()
+        const withdrawAccountId = accountid.toString()
+
+        return extrinsicSigner === withdrawAccountId ? (fee as Balance).toBigInt() : BigInt(0)
+    }
+
+    const eventRecordDeposit = extrinsic.events.find((event) => {
+        return event.event.method == "Deposit" && event.event.section == "balances"
+    })
+
+    const eventRecordTreasury = extrinsic.events.find((event) => {
+        return event.event.method == "Deposit" && event.event.section == "treasury"
+    })
+
+    if(eventRecordDeposit) {
+        const {event: {data: [, fee]}}= eventRecordDeposit
+
+        depositFees = (fee as Balance).toBigInt()
+    }
+    if(eventRecordTreasury) {
+        const {event: {data: [fee]}}= eventRecordTreasury
+
+        treasuryFees = (fee as Balance).toBigInt()
+    }
+
+    const totalFees = depositFees + treasuryFees
+
+    return totalFees
+}
+
 export async function handleTransfer(event: SubstrateEvent): Promise<void> {
-    const { KARURA: {
-        name, decimals
-    } } = tokens
+
     const {
         event: {
             data: [from, to, amount],
@@ -43,4 +91,43 @@ export async function handleTransfer(event: SubstrateEvent): Promise<void> {
     transferInfo.decimals = expendedDecimals;
 
     await transferInfo.save();
+}
+
+export async function handleFailedTransfers(extrinsic: SubstrateExtrinsic): Promise<void> {
+    const { isSigned } = extrinsic.extrinsic;
+
+    if(isSigned){
+        if(extrinsic.success){
+            return null
+        }
+
+        const method = extrinsic.extrinsic.method;
+        const events = ["transfer", "transferKeepAlive"]
+
+        if(method.section == "balances" && events.includes(method.method)){
+            const [to, amount] = method.args;
+            const from = extrinsic.extrinsic.signer;
+            const expendedDecimals = BigInt("1" + "0".repeat(decimals))
+            const blockNo = extrinsic.block.block.header.number.toNumber();
+            const extrinsicHash = extrinsic.extrinsic.hash.toString();
+            const transformedAmount = (amount as Balance).toBigInt();
+            const timestamp = extrinsic.block.timestamp;
+            await ensureAccounts([from.toString(), to.toString()]);
+
+            const transferInfo = new Transfer(`${blockNo}-${extrinsic.idx}`);
+
+            transferInfo.token = name;
+            transferInfo.fromId = from.toString();
+            transferInfo.toId = to.toString();
+            transferInfo.timestamp = timestamp;
+            transferInfo.extrinsicHash = extrinsicHash;
+            transferInfo.amount = transformedAmount;
+            transferInfo.fees = calculateFees(extrinsic)
+            transferInfo.status = false;
+            transferInfo.decimals = expendedDecimals;            
+
+            await transferInfo.save();
+
+        }
+    }
 }
